be2799d2a9ff03b8790e203c77315704ffcbc6ed
mirdan <syahidmirdan@gmal.com>
2020-01-09 18:21:58 +0700

    Add Sidebar text styles

--- src/common/Sidebar.js
+++ src/common/Sidebar.js
@@ -6,9 +6,9 @@ import { Link, IndexLink } from 'react-router';
 const Sidebar = () => {
     return (
         <div className="sidebar">
-            <div><Link to="#administrator" activeClassName="active">Administrator</Link></div>
-            <div><Link to="#menu" activeClassName="active"> Menu and Order</Link></div>
-            <div><Link to="#cashier" activeClassName="active"> Cashier</Link></div>
+            <div className="sidebar-text-box"><Link to="#administrator" activeClassName="active" className="sidebar-text" >Administrator</Link></div>
+            <div className="sidebar-text-box"><Link to="#menu" activeClassName="active" className="sidebar-text"> Menu and Order</Link></div>
+            <div className="sidebar-text-box"><Link to="#cashier" activeClassName="active" className="sidebar-text"> Cashier</Link></div>
         </div>
     );
 };
