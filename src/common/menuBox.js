e700944eeeeb43c29c2e74849cad705f88299785
mirdan <syahidmirdan@gmal.com>
2020-01-09 10:11:07 +0700

    Add props Variable for MenuBox component

--- src/common/menuBox.js
+++ src/common/menuBox.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
 
-const MenuBox = () => {
+const MenuBox = ({dataMenu, plusQty, minusQty} ) => {
     return (
         <div className="menu-box"> 
-            <div className="border img"> Picture </div>
+            <div className="border img">Picture</div>
             <div className="detail-menu">
-                <h4 className="dish-name "> Ayam Taliwang Komplit </h4>
+            <h4 className="dish-name "> {dataMenu.name}</h4>
                 <p className="description"> 
-                    Nasi, ayam taliwang, sambel, tahu dan tempe 
+                    {dataMenu.description}
                 </p>               
             </div>
             <div className="price-qty">
-                <h5 className="price "> 23.000</h5>
-                <div className="qty-box "> - [ 0 ] + </div>
+                <h5 className="price "> {dataMenu.price} </h5>
+                <div className="qty-box "> <button onClick={minusQty}>-</button>  {dataMenu.quantity}  <button onClick={plusQty}>+</button> </div>
             </div>
 
         </div>
