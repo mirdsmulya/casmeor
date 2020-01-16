bc955583f91c299f13de500348ae792367ea936e
mirdan <syahidmirdan@gmal.com>
2020-01-16 16:17:13 +0700

    Activate <Link /> for jumpin to another page purpose

--- src/common/Sidebar.js
+++ src/common/Sidebar.js
@@ -1,14 +1,20 @@
 
 import React from 'react';
 import { render } from 'react-dom';
-import { Link, IndexLink } from 'react-router';
+import { Link, IndexLink } from 'react-router-dom';
 
-const Sidebar = () => {
+const Sidebar = ({onClick}) => {
     return (
         <div className="sidebar">
-            <div className="sidebar-text-box"><Link to="#administrator" activeClassName="active" className="sidebar-text" >Administrator</Link></div>
-            <div className="sidebar-text-box"><Link to="#menu" activeClassName="active" className="sidebar-text"> Menu and Order</Link></div>
-            <div className="sidebar-text-box"><Link to="#cashier" activeClassName="active" className="sidebar-text"> Cashier</Link></div>
+            <div className="sidebar-text-box">
+                <Link to="administrator" activeClassName="active" className="sidebar-text" >Administrator</Link>
+            </div>
+            <div className="sidebar-text-box">
+                <Link to="/" activeClassName="active" className="sidebar-text" > Menu and Order</Link>
+            </div>
+            <div className="sidebar-text-box">
+                <Link to="cashier" activeClassName="active" className="sidebar-text" > Cashier</Link>
+            </div>
         </div>
     );
 };
