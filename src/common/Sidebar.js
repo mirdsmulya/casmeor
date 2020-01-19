cd94a9ae62bdb27053a6a9a04f77dacd85235f4e
mirdan <syahidmirdan@gmal.com>
2020-01-19 19:23:48 +0700

    Change Sidebar to the independent entity and onClick for button

--- src/common/Sidebar.js
+++ src/common/Sidebar.js
@@ -5,15 +5,21 @@ import { Link, IndexLink } from 'react-router-dom';
 
 const Sidebar = ({onClick}) => {
     return (
-        <div className="sidebar">
-            <div className="sidebar-text-box">
-                <Link to="administrator" activeClassName="active" className="sidebar-text" >Administrator</Link>
-            </div>
-            <div className="sidebar-text-box">
-                <Link to="/" activeClassName="active" className="sidebar-text" > Menu and Order</Link>
-            </div>
-            <div className="sidebar-text-box">
-                <Link to="cashier" activeClassName="active" className="sidebar-text" > Cashier</Link>
+ 
+        <div className="fixed-sidebar">
+				<div className="sticky">
+				<div className="header-logo">LOGO</div>
+                     <div className="sidebar">
+                            <div className="sidebar-text-box">
+                                <Link  activeClassName="active" className="sidebar-text" onClick={onClick} name="admin" >Administrator</Link>
+                            </div>
+                            <div className="sidebar-text-box">
+                                <Link to="/"  activeClassName="active" className="sidebar-text" onClick={onClick} name="menu"> Menu and Order</Link>
+                            </div>
+                            <div className="sidebar-text-box">
+                                <Link to="cashier" activeClassName="active" className="sidebar-text" > Cashier</Link>
+                            </div>
+                </div>
             </div>
         </div>
     );
