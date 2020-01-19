342298918677e34c6a89d8a079633710645cb631
mirdan <syahidmirdan@gmal.com>
2020-01-19 19:31:01 +0700

    Add cashier page sidebar

--- src/component/CashierPage.js
+++ src/component/CashierPage.js
@@ -4,12 +4,7 @@ import Sidebar from '../common/Sidebar';
 const CashierPage = () => {
     return(
         <div className="main">
-            <div className="fixed-sidebar">
-				<div className="sticky">
-				<div className="header-logo">LOGO</div>
-				<Sidebar/>
-				</div>
-			</div>
+			<Sidebar/>
         </div>
     );
 };
