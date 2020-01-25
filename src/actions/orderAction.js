2428c10acbcbefaf9e8e971c376244a343970a99
mirdan <syahidmirdan@gmal.com>
2020-01-26 01:27:53 +0700

    Add orderAction initial code

---
+++ src/actions/orderAction.js
@@ -0,0 +1,46 @@
+import OrderApi from '../api/listOrderApi';
+import * as types from './actionTypes';
+
+
+
+export function loadOrderSuccess(orders) {
+    return {type: types.LOAD_ORDER_SUCCESS, orders};
+}
+
+export function saveOrderSuccess(orders) {
+    return {type: types.SAVE_ORDER_SUCCESS, orders};
+}
+
+export function deleteOrderSuccess(orders) {
+    return {type: types.DELETE_ORDER_SUCCESS, orders};
+}
+
+
+
+export function loadOrder() {
+    return function(dispatch) {
+        debugger;
+        return OrderApi.getAllOrder().then( orders => {
+            dispatch(loadOrderSuccess(orders));
+        }).catch(error => {
+            throw(error);
+        });
+    };
+}
+
+export function saveOrder(newOrder) {
+    return function(dispatch) {
+        debugger;
+        return OrderApi.saveOrder(newOrder).then( orders => {
+            dispatch(saveOrderSuccess(orders));
+        }).catch(error => {
+            throw(error);
+            
+        });
+    };
+}
+
+
+
+
+
