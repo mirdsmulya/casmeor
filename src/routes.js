4c23312d7580a9a852bd6bab5be1b54abe6c2657
mirdan <syahidmirdan@gmal.com>
2020-01-26 01:24:03 +0700

    Add page routes

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+import App from './component/App';
+import MainMenuPage from './component/MainMenuPage';
+import AdministratorPage from './component/AdministratorPage';
+import { BrowserRouter } from 'react-router-dom';
+import CashierPage from './component/CashierPage';
+import AccountPage from './component/AccountPage';
+
+
+export default (
+
+    <Route path="/" component={App} >
+		<IndexRoute component={MainMenuPage} />
+        <Route path="setting" component={AdministratorPage} />
+        <Route path="cashier" component={CashierPage} />
+        <Route path="account" component={AccountPage} /> 
+
+	</Route>
+      
+);
