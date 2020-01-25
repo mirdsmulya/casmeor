beee5bd9dffcf6eb9755047687c5892c65e20c89
mirdan <syahidmirdan@gmal.com>
2020-01-26 01:42:27 +0700

    Change from route schema to class-child schema

--- src/component/App.js
+++ src/component/App.js
@@ -2,17 +2,27 @@ import React from 'react';
 import MainMenuPage from './MainMenuPage';
 import AdministratorPage from './AdministratorPage';
 import CashierPage from './CashierPage';
-import { Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import Sidebar from '../common/Sidebar';
 
 
-const App = () => {
-    return(
-        <div>
-            <Route exact path="/" component={MainMenuPage} />
-            <Route path="/administrator" component={AdministratorPage} /> 
-            <Route path="/cashier" component={CashierPage} />
-        </div>
-    );
-};
+class App extends React.Component {
+    
+    
+	render() {
+        console.log(this.props.children);
+        debugger;
+		return(
+            <BrowserRouter>
+			<div className="">
+
+				{this.props.children}
+
+			</div>
+            </BrowserRouter>
+			);
+	}
+}
+
 
 export default App;
