e545b8f9bef9f45b9b18874ae9b7d7c712562a5b
mirdan <syahidmirdan@gmal.com>
2020-01-26 01:31:26 +0700

    Update for minor change

--- src/index.js
+++ src/index.js
@@ -3,15 +3,24 @@ import { render } from 'react-dom';
 import './styles/styles.scss';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/toastr/build/toastr.min.css';
-
+import {loadMenu} from './actions/menuAction';
+import {loadOrder} from './actions/orderAction';
 import App from './component/App';
-import { BrowserRouter as Router } from 'react-router-dom';
+//import { BrowserRouter as Router, browserHistory } from 'react-router-dom';
+import configureStore from './stores/configureStrore';
+import { Provider } from 'react-redux'; 
+import routes from './routes';
+import { Router, browserHistory } from 'react-router';
 
+
+const store = configureStore();
+store.dispatch(loadMenu());
+store.dispatch(loadOrder());
 
 
 render(
-	<Router>
-		<App />
-	</Router> ,
+	<Provider store={store}>
+		<Router history={browserHistory} routes={routes} />
+	</Provider>,
 	document.getElementById('app')
 );
