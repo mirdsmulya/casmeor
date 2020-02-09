20286d7968bdfb8801a66c2faa61db46d1b78bb3
mirdan <syahidmirdan@gmal.com>
2020-02-09 11:39:05 +0700

    Add confirmOrder() orderDetails() dataInputChange() userCheck()

--- src/component/CashierPage.js
+++ src/component/CashierPage.js
@@ -1,35 +1,49 @@
 import React from 'react';
 import Sidebar from '../common/Sidebar';
 import OrderBoard from './orderBoard';
+import OrderDetails from './OrderDetails';
+import OrderHistory from './OrderHistory';
+import ListOrderApi from '../api/listOrderApi';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import * as menuAction from '../actions/menuAction';
-import Toastr from 'toastr'
+import Toastr from 'toastr';
 
 class CashierPage extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.state = {
-            dataOrder: this.props.order,
-            totalPrice: 0
+            dataOrder: [],
+            totalPrice: 0,
+            orderDetails: {},
+            orderHistory: []
         };
-    this.confirmPayment = this.confirmPayment.bind(this);
+    this.confirmOrder = this.confirmOrder.bind(this);
+    this.orderDetails = this.orderDetails.bind(this);
+    this.dataInputChange = this.dataInputChange.bind(this);
     }
 
-    componentWillMount() {
-        this.calculateTotalPrice();
+    componentDidMount() {
+        ListOrderApi.getAllHistoryOrder()
+        .then((result)=> this.setState({orderHistory: result}));
+        debugger;
+
     }
 
     componentWillReceiveProps(nextProps) {
-		if (this.props.orders !== nextProps.orders) {
-			this.setState({dataOrder: Object.assign({}, nextProps.orders)});
+		if (this.props.order !== nextProps.order) {
+            this.setState({dataOrder: Object.assign({}, nextProps.order)});
+           
         }
+        this.setState({dataOrder: this.props.order})
+        this.calculateTotalPrice();
+        this.orderDetails();
 		debugger;
 	}
 
     calculateTotalPrice() {
 		setTimeout( () => {
-			let dataOrder = Object.assign([], this.state.dataOrder);
+			let dataOrder = Object.assign([], this.props.order);
 			let lengthOrder = dataOrder.length;
 			let totalPrice =0;
 			for (let i=0; i < lengthOrder; i++) {
@@ -41,8 +55,6 @@ class CashierPage extends React.Component {
             if (totalPrice == 0) {
                 this.setState({order: 'hide'});
             }
-            //this.props.orderAction.saveOrder(Object.assign([], this.state.dataOrder));
-
 
 			this.setState({totalPrice: totalPrice});
 			debugger;
@@ -52,8 +64,57 @@ class CashierPage extends React.Component {
 		debugger;
     }
 
-    confirmPayment() {
-        let a;
+    confirmOrder() {
+        let data = Object.assign({}, this.state.orderDetails);
+        let totalAmount = this.state.totalPrice;
+        let lastOrderNumb = this.state.orderHistory.slice(-1)[0];
+        data['totalAmount'] = totalAmount;
+        debugger;
+        if (data['orderNumber'] != lastOrderNumb['orderNumber']) {
+            ListOrderApi.saveOrderHistory(data)
+            .then(orders => { 
+                this.setState({orderHistory: orders});
+                Toastr.success('Order confirmed!');
+            });
+            return;
+            
+        }
+        Toastr.error('Order already made!');
+    }
+
+    orderDetails() {
+        setTimeout(() => {
+
+            const newDate = new Date();
+            const dateString = newDate.getDate() + "/" + (newDate.getMonth()+1)  + "/" + newDate.getFullYear();
+            const time = newDate.getTime();
+            const cashier = sessionStorage.getItem('currentUserLogin');
+            const history = Object.assign({}, this.state.orderHistory.slice(-1)[0]);
+            const orderNumb = history['orderNumber']+ 1;
+            const orderDetail = {
+                timeOrder: time,
+                cashierIdentity: cashier, 
+                currentDate: dateString, 
+                orderNumber: orderNumb, 
+                table: "",
+                name: "",
+                status: "Unpaid",
+                totalAmount: 0 
+            };
+        debugger;
+        this.setState({orderDetails: orderDetail});
+        }, 0);        
+    }
+
+
+    dataInputChange(event) {
+        
+        const field = event.target.name;
+        let data = Object.assign({}, this.state.orderDetails);
+        data[field] = event.target.value;
+        debugger;
+        return this.setState({orderDetails: data});
+
     }
 
     userCheck() {
@@ -68,20 +129,35 @@ class CashierPage extends React.Component {
         console.log(this.state.dataOrder);
         console.log(this.props.order);
         console.log(this.props.menu);
+        console.log(this.orderDetails);
+        //this.orderDetails();
         this.userCheck();
         debugger;
         
         return(
             <div className="main">
                 <Sidebar/>
+                <div>
+                <div>Order Details</div>
+                <OrderDetails 
+                    orderDetails={this.state.orderDetails}
+                    tableChange={this.dataInputChange}
+                />
                 <OrderBoard
                     
                     dataOrder={this.props.order}
                     totalPrice={this.state.totalPrice}
                     orderLine="cashier-line" 
-                    buttonText="Confirm Payment"
-                    confirmOrder={this.confirmPayment}
+                    buttonText="Confirm Order"
+                    confirmOrder={this.confirmOrder}
                     totalPrice={this.state.totalPrice}
+                />
+
+                
+                </div>
+                <OrderHistory 
+                    orderHistory={this.state.orderHistory}
+                
                 />
             </div>
         );
