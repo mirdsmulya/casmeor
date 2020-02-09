66eca817baa2ba33b3e4438add83929bb62a2a7b
mirdan <syahidmirdan@gmal.com>
2020-02-09 11:24:00 +0700

    Add getAllHistoryOrder and saveOrderHistory

--- src/api/listOrderApi.js
+++ src/api/listOrderApi.js
@@ -1,6 +1,17 @@
 
 let order = [];
-
+let orderHistory = [
+    {
+        timeOrder: "",
+        cashierIdentity: "mirdsm", 
+        currentDate: "4/2/2020 ", 
+        orderNumber: 0, 
+        table: "6",
+        name: "Caca",
+        status: "Unpaid",
+        totalAmount: 20000  
+    }
+];
 
 
 class OrderApi {
@@ -21,6 +32,27 @@ class OrderApi {
         return new Promise((resolve, reject) => {
             setTimeout(() => {
                 resolve(Object.assign([], news));
+                debugger;
+            },0);
+        });
+    }
+
+    static getAllHistoryOrder() {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(Object.assign([], orderHistory));
+                debugger;
+            },0);
+        });
+    }
+
+    static saveOrderHistory(newOrder) {
+        debugger;
+
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                orderHistory = [...orderHistory, newOrder]
+                resolve(Object.assign([], orderHistory));
                 debugger;
             },0);
         });
