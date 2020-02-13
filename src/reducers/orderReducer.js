92fb626070d1d8811dd2cc5092fc68987c7c09bb
mirdan <syahidmirdan@gmal.com>
2020-02-13 12:11:05 +0700

    Add UPDATE_ORDER_SUCCESS types

--- src/reducers/orderReducer.js
+++ src/reducers/orderReducer.js
@@ -10,6 +10,9 @@ export default function orderReducer(state = initialState.orders, action) {
             return action.orders;
         case types.SAVE_ORDER_SUCCESS:
             return action.orders;
+        case types.UPDATE_ORDER_SUCCESS:
+            return action.orders;
+
         default:
             return state;
     }
