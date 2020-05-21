bcd52be8ed819dbc8a1e86668747c3b4d88bc66b
mirdan <syahidmirdan@gmal.com>
2020-05-21 11:54:58 +0700

    Add getItemMenu() method - Modified updateOrder() to HTTP PUT method - Modified saveOrder() to HTTP POST method - Modified getAllOrder to HTTP GET method

--- src/api/listOrderApi.js
+++ src/api/listOrderApi.js
@@ -1,42 +1,47 @@
 
-let order = [];
 let orderHistory = [
     {
-        timeOrder: "",
         cashierIdentity: "mirdsm", 
-        currentDate: "4/2/2020 ", 
-        orderNumber: 0, 
-        tableNumber: "6",
-        name: "Caca",
-        paymentStatus: "Unpaid",
-        totalAmount: 20000,
+        currentDate: "4/2/2020 ",
         id: "123123",
-        orderList: []  
+        name: "Caca",
+        orderNumber: 0, 
+        paymentStatus: "Unpaid",
+        tableNumber: "6",
+        timeOrder: "",
+        totalAmount: 20000,
+        orderList: []
     }
 ];
 
 
 class OrderApi {
     static getAllOrder() {
-        debugger;
         return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve(Object.assign([], orderHistory));
-                debugger;
-            },0);
+            const urlFetch = fetch('http://localhost:3000/orders');
+            urlFetch.then( res => {
+                if (res.status === 200) { return res.json(); } 
+            }).then(result => resolve(result.values));
         });
+        
     }
 
-
-    static saveOrder(newOrder) {
-        debugger;
-        //let news = Object.assign([], newOrder)
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                orderHistory = [...orderHistory, newOrder];
-                resolve(Object.assign([], orderHistory));
-                debugger;
-            },0);
+    static saveOrder(newOrder, currentOrder) {
+        return new Promise((resolve, reject) => {            
+            const postMethod = {
+                method: 'POST', 
+                mode: 'cors', 
+                cache: 'no-cache', 
+                headers: {
+                'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(newOrder) 
+            };
+            
+            const urlFetch = fetch('http://localhost:3000/saveOrder', postMethod);
+            urlFetch.then( res => {
+                const result = res.status === 200 ? resolve([...currentOrder, newOrder]) : resolve(currentOrder);
+            });
         });
     }
 
@@ -44,33 +49,52 @@ class OrderApi {
         return new Promise((resolve, reject) => {
             setTimeout(() => {
                 resolve(Object.assign([], orderHistory));
-                debugger;
             },0);
         });
     }
 
     static saveOrderHistory(newOrder) {
-        debugger;
-
         return new Promise((resolve, reject) => {
             setTimeout(() => {
                 orderHistory = [...orderHistory, newOrder];
                 resolve(Object.assign([], orderHistory));
-                debugger;
             },0);
         });
     }
 
-    static updateOrder(updatedOrder) {
+    static updateOrder(updatedOrder, orders) {
         return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                let orderIndex = orderHistory.findIndex((a) => a.id == updatedOrder.id);
-                orderHistory.splice(orderIndex, 1, updatedOrder);
-                resolve(Object.assign([], orderHistory));
+            const putMethod = {
+                method: 'PUT', 
+                mode: 'cors', 
+                cache: 'no-cache', 
+                headers: {
+                'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(updatedOrder) 
+            };
+            
+            const urlFetch = fetch('http://localhost:3000/updateOrder', putMethod);
+            urlFetch.then( res => {
+                if (res.status === 200) {
+                    let orderIndex = orders.findIndex((a) => a.id == updatedOrder.id);
+                    orders.splice(orderIndex, 1, updatedOrder);
+                    resolve(Object.assign([], orders));   
+                }
             });
-
         });
         
+    }
+
+    static getItemMenu() {
+        return new Promise((resolve, reject) => {
+            const urlFetch = fetch('http://localhost:3000/getItemOrder');
+            urlFetch.then( res => {
+                if (res.status === 200) { return res.json(); }
+            })
+            .then( result => {
+                resolve(result.values); });
+        });
     }
 }
 
