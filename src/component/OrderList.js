501eaec8b66523b8daa1a98ac8cf15676a6f7dae
mirdan <syahidmirdan@gmal.com>
2020-07-06 22:11:59 +0700

    Delete print button and edit Add Order into Edit Order button

--- src/component/OrderList.js
+++ src/component/OrderList.js
@@ -15,9 +15,8 @@ const OrderList = ({orders, printBill, addOrder, confirmPayment, deleteOrder}) =
                 <td>{orders.totalAmount} </td>
                 <td>{orders.paymentStatus} </td>
                 <td>{orders.cashierIdentity} </td>
-                <td><button className="btn" onClick={addOrder} name={orders.id} disabled={buttonDisable}>Add Order</button> </td>
+                <td><button className="btn" onClick={addOrder} name={orders.id} disabled={buttonDisable}>Edit Order</button> </td>
                 <td><button className="btn" onClick={confirmPayment} name={orders.id} disabled={buttonDisable}>Confirm Payment</button> </td>
-                <td><button className="btn" onClick={printBill} name={orders.id}>Print Bill</button> </td>
                 <td><button className="btn btn-danger" onClick={deleteOrder} name={orders.id} >Delete</button> </td>
 
             </tr>
